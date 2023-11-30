0288ea2a999dc109d09ad293188e0775a1dd3dbe
GritsaevTim <timgri93@gmail.com>
2023-11-30 16:03:53 +0300

    fd

--- src/model/hifigan_model.py
+++ src/model/hifigan_model.py
@@ -25,9 +25,11 @@ class HiFiGANModel(BaseModel):
                 mpd_out, mpd_feature_map = mpd(wav)
                 feature_maps += mpd_feature_map
 
+                print(f"{mpd_out.shape=}")
                 mpds_out.append(mpd_out)
 
             msd_out, msd_feature_maps = self.msd(pred)
+            print(f"{msd_out.shape=}")
             feature_maps += msd_feature_maps
 
             return torch.cat([*mpds_out, msd_out]), feature_maps
