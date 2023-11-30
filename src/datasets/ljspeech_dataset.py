67e2dbb76b28acca3f43e4d4a137b3db6b70ccf8
GritsaevTim <timgri93@gmail.com>
2023-11-30 19:58:19 +0300

    fd

--- src/datasets/ljspeech_dataset.py
+++ src/datasets/ljspeech_dataset.py
@@ -1,13 +1,15 @@
-import os
+import random
 from pathlib import Path
 from tqdm import tqdm
 
 import torchaudio
 from torch.utils.data import Dataset
 
+from src.utils import DEFAULT_SR
+
 
 class LJSpeechDataset(Dataset):
-    def __init__(self, dir, limit=None, **kwargs):
+    def __init__(self, dir, limit=None, max_len=None, **kwargs):
         dir = Path(dir)
 
         # self.texts = []
@@ -20,6 +22,7 @@ class LJSpeechDataset(Dataset):
             self.wavs_path.append(str(wav_path))
         self.wavs_path.sort()
         self.wavs_path = self.wavs_path[:limit]
+        self.max_len = max_len
 
     def __len__(self):
         return len(self.wavs_path)
@@ -27,4 +30,7 @@ class LJSpeechDataset(Dataset):
     def __getitem__(self, idx):
         wav, _ = torchaudio.load(self.wavs_path[idx])
         # return {"wav": wav, "text": self.texts[idx]}
+        if self.max_len:
+            start = random.randint(0, max(0, wav.shape[-1] - self.max_len))
+            wav = wav[start : start + self.max_len * DEFAULT_SR]
         return {"wav": wav}
