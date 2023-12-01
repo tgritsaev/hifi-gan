3352396711a200b8cfd2c7fdafb6da8d36e32e69
GritsaevTim <timgri93@gmail.com>
2023-12-01 13:55:26 +0300

    fd

--- src/collate_fn/collate.py
+++ src/collate_fn/collate.py
@@ -5,15 +5,44 @@ from src.utils.mel_spectrogram import MelSpectrogram, MelSpectrogramConfig
 wav2mel = MelSpectrogram(MelSpectrogramConfig())
 
 
+def pad_1D_tensor(inputs, PAD=0):
+    def pad_data(x, length, PAD):
+        x_padded = F.pad(x, (0, length - x.shape[0]), value=PAD)
+        return x_padded
+
+    max_len = max((len(x) for x in inputs))
+    padded = torch.stack([pad_data(x, max_len, PAD) for x in inputs])
+
+    return padded
+
+
+def pad_2D_tensor(inputs, maxlen=None):
+    def pad(x, max_len):
+        if x.size(-1) > max_len:
+            raise ValueError("not max_len")
+
+        x_padded = F.pad(x, (0, max_len - x.size(-1), 0, 0))
+        return x_padded
+
+    if maxlen:
+        output = torch.stack([pad(x, maxlen) for x in inputs])
+    else:
+        max_len = max(x.size(-1) for x in inputs)
+        output = torch.stack([pad(x, max_len) for x in inputs])
+
+    return output
+
+
 def collate_fn(batch: List[dict]):
     wavs = torch.stack([item["wav"] for item in batch]).squeeze(1)
-    # new_length = (wavs.shape[-1] // 256) * 256
-    # wavs = wavs[..., :new_length]
-
-    # mels = []
-    # for i in range(wavs.shape[0]):
-    #     mels.append(wav2mel(wavs[i]))
-    # mels = pad_2D_tensor(mels).squeeze(1)
     mels = wav2mel(wavs)
 
+    output = {}
+    output["wav_gt"] = pad_1D_tensor([item["wav_gt"].squeeze(0) for item in batch]).unsqueeze(1)
+    output["mel_gt"] = pad_2D_tensor([item["mel_gt"] for item in batch])
+
+    print("!!!!!", wavs.shape, mels.shape)
+    print("?????", output["wav_gt"].shape, output["mel_gt"].shape)
+
     return {"target": wavs, "mel": mels}
+    return output
