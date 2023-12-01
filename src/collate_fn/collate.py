f16bc401efae94eac88048eddc67336ee9f492e5
GritsaevTim <timgri93@gmail.com>
2023-12-01 14:14:16 +0300

    fd

--- src/collate_fn/collate.py
+++ src/collate_fn/collate.py
@@ -36,18 +36,6 @@ def pad_2D_tensor(inputs, maxlen=None):
 
 def collate_fn(batch: List[dict]):
     wavs = torch.stack([item["wav"] for item in batch])
-    mels = wav2mel(wavs)
-    print("!!!!!", wavs.shape, mels.shape)
-
-    wavs = pad_1D_tensor([item["wav"].squeeze(0) for item in batch]).unsqueeze(1)
-    new_length = (wavs.shape[-1] // 256) * 256
-    wavs = wavs[..., :new_length]
-
-    # mels = []
-    # for i in range(wavs.shape[0]):
-    #     mels.append(wav2mel(wavs[i]))
-    # mels = pad_2D_tensor(mels).squeeze(1)
-    mels = pad_2D_tensor([mel for mel in wav2mel(wavs)]).squeeze(1)
-    print("?????", wavs.shape, mels.shape)
+    mels = wav2mel(wavs).squeeze(1)
 
     return {"target": wavs, "mel": mels}
