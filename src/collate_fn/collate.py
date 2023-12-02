37706829b26ef5aa16d438c2d332d74965c8fe77
GritsaevTim <timgri93@gmail.com>
2023-12-02 23:12:37 +0300

    da

--- src/collate_fn/collate.py
+++ src/collate_fn/collate.py
@@ -8,6 +8,5 @@ wav2mel = MelSpectrogram(MelSpectrogramConfig())
 def collate_fn(batch: List[dict]):
     wavs = torch.stack([item["wav"] for item in batch])
     mels = wav2mel(wavs).squeeze(1)
-    print("!!!!!", mels.shape)
 
     return {"target": wavs, "mel": mels}
